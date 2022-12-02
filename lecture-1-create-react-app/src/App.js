4a6501c9eb6b76429ce84ebae71d5ab5ca42c9bd
= <astakhov00@gmail.com>
2022-12-01 21:10:59 -0500

    reset-decrease

--- lecture-1-create-react-app/src/App.js
+++ lecture-1-create-react-app/src/App.js
@@ -7,11 +7,23 @@ export function App() {
         setCount(count + 1)
     }
 
+    function resetHandler() {
+        setCount(0)
+    }
+
+    function decrementHandler() {
+        setCount(count - 1)
+    }
+
     return (
         <div>
             Hello World!
             <div>
                 <button onClick={clickHandler}>Click to increase</button>
+                <div>{count}</div>
+                <button onClick={resetHandler}>Reset</button>
+                <div>{count}</div>
+                <button onClick={decrementHandler}>Click to decrease</button>
                 <div>{count}</div>
             </div>
         </div>
