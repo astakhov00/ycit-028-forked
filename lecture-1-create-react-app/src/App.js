ad5ca30fcefc334b807facaca216667ced71f319
= <astakhov00@gmail.com>
2022-12-01 21:08:35 -0500

    reset-decrease

--- lecture-1-create-react-app/src/App.js
+++ lecture-1-create-react-app/src/App.js
@@ -1,3 +1,19 @@
+import { useState } from "react"
+
 export function App() {
-    return <div></div>
+    const [count, setCount] = useState(0)
+
+    function clickHandler() {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            Hello World!
+            <div>
+                <button onClick={clickHandler}>Click to increase</button>
+                <div>{count}</div>
+            </div>
+        </div>
+    )
 }
